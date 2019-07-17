c6dacb3277bec6a0b9ad4d39cfc823eed4feb000
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-07-17 21:25:09 +0900

    triangle.rbの実装完了

--- triangle.rb
+++ triangle.rb
@@ -1,6 +1,24 @@
 class Triangle
   def self.shape_of_triangle_for(arg_0, arg_1, arg_2)
-    arg_0 + arg_1 + arg_2
+    side_0 = arg_0.delete(",").to_i
+    side_1 = arg_1.delete(",").to_i
+    side_2 = arg_2.to_i
+
+    if (side_0 - side_1).abs < side_2 and side_2 < side_0 + side_1
+
+      side_ary = [side_0, side_1, side_2]
+
+      if side_ary.uniq.count == 1
+        '正三角形ですね！'
+      elsif side_ary.uniq.count == 2
+        '二等辺三角形ですね！'
+      else
+        '不等辺三角形ですね！'
+      end
+
+    else
+      '三角形じゃないです＞＜'
+    end
   end
 end
 
